7ee4701a8c29948402ff87735b3cd68f263e09ac
janithDakshitha <107628399+janithDakshitha@users.noreply.github.com>
2022-11-28 18:16:09 +0530

    Add javascript for univdashboard

--- Project-G06/wwwroot/js/site.js
+++ Project-G06/wwwroot/js/site.js
@@ -2,3 +2,48 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+console.log("hi")
+let addDegree1 = document.getElementById("DegreeButton")
+addDegree1.addEventListener("click", addnewDegree)
+function addnewDegree() {
+    console.log("hi1")
+    var Row = document.createElement('div')
+    Row.classList.add('cart-row')
+    var cdegree = document.getElementsByClassName('DegreeName')[0]
+
+    var DegreeAdd = `
+            <h2>Degree</h2>
+    <label for="Dname"> Degree Name:</label>
+    <input type="text" id="Dname" name="Dname" value=""><br>
+
+    <label for="DType"> Degree Type:</label>
+    <input type="text" id="DType" name="DType" value=""><br>
+
+    <label for="DAdRe"> Admission Requirenments:</label>
+    <input type="text" id="DAdRe" name="DAdRe" value=""><br>
+
+    <label for="DCC"> Course Content:</label>
+    <input type="text" id="DCC" name="DCC" value=""><br>
+
+    <label for="DNSem"> Number of semesters:</label>
+    <input type="number" id="DNSem" name="DNSem" value=""><br>
+    
+    <label for="DCOp"> Career Opportunities:</label><br>
+    <textarea name="textarea" rows="3" cols="100">Write something here</textarea><br>
+
+    <label for="DCFe"> Course Fee:</label>
+    <input type="text" id="DCC" name="DCC" value=""><br>
+
+    <label for="DUGC"> UGC Approval:</label>
+    <input type="text" id="DUGC" name="DUGC" value=""><br>
+
+    <label for="DAUni"> Affiliated Campus Offering the degree:</label>
+    <input type="text" id="DAUni" name="DAUni" value="">
+    <input type="number" id="DAUni" name="DAUni" value=""><br>
+
+    <label for="DAProg">Academic Progression:</label><br>
+    <textarea name="textarea" rows="2" cols="100">Write something here</textarea><br>
+            `
+    Row.innerHTML = DegreeAdd
+    cdegree.append(Row)
+}
