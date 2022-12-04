bdcbdd69a1ad0b77fe821dab844607f31b066f1b
Dulangi karunasekara <dulangi2002@gmail.com>
2022-12-04 15:03:07 +0530

    adding nav element

--- Project-G06/Controllers/HomeController.cs
+++ Project-G06/Controllers/HomeController.cs
@@ -43,6 +43,14 @@ namespace Project_G06.Controllers
             return View();
         }
 
+        public IActionResult unireg()
+        {
+            return View();
+        }
+
+
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
