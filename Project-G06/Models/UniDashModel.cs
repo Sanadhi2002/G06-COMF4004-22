13ed2f672170f8dfc699efee68fec6044439bdc5
janithDakshitha <107628399+janithDakshitha@users.noreply.github.com>
2022-12-05 11:22:49 +0530

    create Unidash Model

---
+++ Project-G06/Models/UniDashModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Project_G06.Models
+{
+    public class UniDashModel
+    {
+        public int ID { get; set; }
+        public string UniName { get; set; }
+        public string UniDetails { get; set; }
+        public string Faculty { get; set; }
+        public string DegreeName { get; set; }
+        public string DegreeType { get; set; }
+        public string  AddmissionReq { get; set; }
+        public string CourseContent { get; set; }
+        public int NumberOfSem { get; set; }
+        public string  CareerOp { get; set; }
+        public string CourseFee { get; set; }
+        public string UGC { get; set; }
+        public string AffUni { get; set; }
+        public string AcademicProg { get; set; }
+    }
+}
