af6a516157255081eb540904f64efe14ecbcd2ec
Dulangi karunasekara <dulangi2002@gmail.com>
2022-12-11 23:42:40 +0530

    adding backend changes

---
+++ Project-G06/Data/UniversitiesDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project_G06.Models;
+
+namespace Project_G06.Data
+{
+    public class UniversitiesDbContext : DbContext
+    {
+        public UniversitiesDbContext(DbContextOptions<UniversitiesDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Universities> Name { get; set; }
+    }
+}
+
---
+++ Project-G06/Models/Universities.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_G06.Models;
+
+public class Universities
+{
+
+    [Key]
+    public string Name { get; set; }
+    [Required]
+
+    public char Email { get; set; }
+    [Required]
+    public char Password { get; set; }
+    [Required]
+
+
+};
