51c9f382b8683b954147c31fc9f0c245fabd1022
janithDakshitha <107628399+janithDakshitha@users.noreply.github.com>
2022-12-13 16:45:48 +0530

    Add UniFaculty database

---
+++ Project-G06/Data/UniFacultyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project_G06.Models;
+
+namespace Project_G06.Data
+{
+    public class UniFacultyDbContext:DbContext
+    {
+        public UniFacultyDbContext(DbContextOptions<UniFacultyDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Uni_FacultyModel> UnifacultyModels { get; set; } //create UniProfileModel  table name with UniProfileModels
+    }
+}
--- Project-G06/Models/Uni_FacultyModel.cs
+++ Project-G06/Models/Uni_FacultyModel.cs
@@ -1,7 +1,11 @@
-﻿namespace Project_G06.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_G06.Models
 {
     public class Uni_FacultyModel
     {
+        [Key]
+        public int Uni_Faculty_ID { get; set; }
         public string Faculty_Name { get; set; }
 
     }
