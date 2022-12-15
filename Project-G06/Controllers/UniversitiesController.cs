ada773be97ceb62819b0ede490b4f54dca419aaf
Dulangi karunasekara <dulangi2002@gmail.com>
2022-12-15 14:55:42 +0530

    add UniversitiesController

---
+++ Project-G06/Controllers/UniversitiesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_G06.Controllers
+{
+    public class UniversitiesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
