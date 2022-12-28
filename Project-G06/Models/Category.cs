dd492bf78c9d9822e9cdf99e32c5254814e2c3e2
Dulangi karunasekara <dulangi2002@gmail.com>
2022-12-28 18:22:20 +0530

    hash password

--- Project-G06/Models/Category.cs
+++ Project-G06/Models/Category.cs
@@ -8,6 +8,13 @@ namespace Project_G06.Models
         public int Id { get; set; }
 
         [Required]
-        public string Name { get; set; }    
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string PasswordSalt { get; set; }
+
     }
 }
