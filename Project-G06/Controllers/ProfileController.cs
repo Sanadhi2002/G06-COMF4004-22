749feb98ae87ce72041e3bec1fa2b3ac09e3a37b
Dulangi karunasekara <dulangi2002@gmail.com>
2022-12-28 12:26:53 +0530

    Uni degrees view for superadmin

---
+++ Project-G06/Controllers/ProfileController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_G06.Data;
+using Project_G06.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Project_G06.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UniProfileDbContext uniProfileDbContext1;
+       
+        public ProfileController(UniProfileDbContext uniProfileDbContext)
+        { 
+            uniProfileDbContext1 = uniProfileDbContext; 
+
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<UniProfileModel> ProfileAttributesList = uniProfileDbContext1.UniProfileModels;
+
+            return View(ProfileAttributesList);
+        }
+    }
+}
