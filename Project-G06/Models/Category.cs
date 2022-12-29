050738bc5a7ae3db075a59735c9bdd20cccc954c
Dulangi karunasekara <dulangi2002@gmail.com>
2022-12-29 20:36:31 +0530

    superadmin

--- Project-G06/Models/Category.cs
+++ Project-G06/Models/Category.cs
@@ -5,16 +5,28 @@ namespace Project_G06.Models
     public class Category
     {
         [Key]
+
         public int Id { get; set; }
 
         [Required]
         public string Name { get; set; }
 
+        [Required]
         public string OtherName { get; set; }
 
+        [Required]
         public string Email { get; set; }
-        public string Password { get; set; }  
-        
+
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+        [Required(ErrorMessage = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
         public string ConfirmPassword { get; set; } 
 
    
