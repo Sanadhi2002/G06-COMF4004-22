56318187181628d0adfeca2f77b3c470e3013da1
janithDakshitha <107628399+janithDakshitha@users.noreply.github.com>
2022-12-30 17:03:59 +0530

    add degreecontentmodel

---
+++ Project-G06/Models/DegreeContentModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project_G06.Models
+{
+    public class DegreeContentModel
+    {
+        [Key]
+        public int DegreeContent_Id { get; set; }
+        public string DegreeContents { get; set; }
+
+        [ForeignKey("DegreeModel")]
+        public int Degree_ID { get; set; }
+        public DegreeModel DegreeModel { get; set; }
+    }
+}
