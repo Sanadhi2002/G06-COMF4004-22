39199e5b7a7f5f6dd08da064b26f371e78fa6edf
Dulangi karunasekara <dulangi2002@gmail.com>
2022-12-30 19:26:27 +0530

    Update model

--- Project-G06/Models/Category.cs
+++ Project-G06/Models/Category.cs
@@ -8,33 +8,27 @@ namespace Project_G06.Models
     {
         [Key]
 
-        public int Id { get; set; }
+        public int U_ID{ get; set; }
         //this is the uni id . it can have many degrees
 
         [Required]
         
-        public string Name { get; set; }
+        public string U_UserName { get; set; }
 
         [Required]
-         public string OtherName { get; set; }
+         public string U_Email { get; set; }
 
-       //[Required]
-       // [Key, ForeignKey()]
-        //public string S_Name { get; set; }
-        [Required]
-        public string Email { get; set; }
+    //[Required]
+    // [Key, ForeignKey()]
+    //public string S_Name { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
 
-
-
-        [Required]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
+    public string U_Password{ get; set; }
 
 
-        [Required(ErrorMessage = "Confirm Password")]
-        [DataType(DataType.Password)]
-        [Compare("Password")]
-        public string ConfirmPassword { get; set; } 
+
+
 
        // public List<classfordegree> Degreespecificfortheuniversity
 
