4186198f970c34903ca0fbcbdc1ce3374ec7aca2
Dulangi karunasekara <dulangi2002@gmail.com>
2022-12-31 13:33:31 +0530

    degree index

---
+++ Project-G06/Controllers/CompareController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_G06.Data;
+
+namespace Project_G06.Controllers
+{
+    public class CompareController : Controller
+    {
+        private readonly WebAdminDbContext _context;
+
+        public CompareController(WebAdminDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
