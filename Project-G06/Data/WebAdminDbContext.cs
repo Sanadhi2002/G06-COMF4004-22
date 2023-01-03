90b48715a9097ee6b6894c85a17b399a422bda6f
janithDakshitha <107628399+janithDakshitha@users.noreply.github.com>
2023-01-03 18:12:13 +0530

    Revert "search"
    This reverts commit e6ad56c5d14c43d618cbb64bc77c56cde030bbc0.

--- Project-G06/Data/WebAdminDbContext.cs
+++ Project-G06/Data/WebAdminDbContext.cs
@@ -1,10 +1,10 @@
-﻿
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Project_G06.Models;
 
 namespace Project_G06.Data
 {
-    public class WebAdminDbContext :DbContext
+    public class WebAdminDbContext : IdentityDbContext
     {
         public WebAdminDbContext(DbContextOptions <WebAdminDbContext>options) : base(options)
         {
--- Project-G06/Data/WebApplication2User.cs
+++
@@ -1,9 +0,0 @@
-﻿using Project_G06.Models;
-
-namespace Project_G06.Data
-{
-    public class WebApplication2User
-    {
-        public virtual Class Class { get; set; }
-    }
-}
