5cf494474d9732b5c99bb207886416d71a1d9a75
Sanadhi2002 <sanadhi2002@gmail.com>
2023-01-03 16:24:28 +0530

    Revert "Add files via upload"
    This reverts commit 89b1ba182a63807916de32852fc6cb77049d6583.

--- Project-G06/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Project-G06/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -21,6 +21,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using Project_G06.Areas.Identity;
+using Project_G06.Data;
 
 namespace Project_G06.Areas.Identity.Pages.Account
 {
