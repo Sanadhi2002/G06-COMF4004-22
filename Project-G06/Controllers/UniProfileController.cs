6931d356963c4fc2e5492f0fcc2d11ab86622019
Hashi <edmandhashi@gmail.com>
2023-01-05 13:47:32 +0530

    add uniprofilecontroller

---
+++ Project-G06/Controllers/UniProfileController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_G06.Data;
+using Project_G06.Models;
+
+namespace Project_G06.Controllers
+{
+    public class UniProfileController : Controller
+    {
+        private readonly WebApplication2DbContext _webApplication2DbContext;
+        public UniProfileController(WebApplication2DbContext webApplication2DbContext)
+        {
+            _webApplication2DbContext= webApplication2DbContext;
+        }
+        public IActionResult UniProfile()
+        {
+            IEnumerable<UniProfileModel> objUniProfileModel = _webApplication2DbContext.UniProfileModel.ToList();
+            return View(objUniProfileModel);
+        }
+    }
+}
