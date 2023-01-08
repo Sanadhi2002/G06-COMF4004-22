09cd7bf5e6349f5c9a5d6268bcd4ebaf60853d91
janithDakshitha <107628399+janithDakshitha@users.noreply.github.com>
2023-01-08 11:47:27 +0530

    update uniprofilemodel

--- Project-G06/Models/UniProfileModel.cs
+++ Project-G06/Models/UniProfileModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Project_G06.Models
@@ -7,12 +8,17 @@ namespace Project_G06.Models
     {
         [Key, ForeignKey("WebApplication2User")]
         public string UserId { get; set; }
+        [DisplayName("University short Name")]
         
         public string? UniShortname { get; set; }
+        
+        [DisplayName("University Full Name")]
         public string? UniFullname { get; set; }
+        [DisplayName("University Details")]
         public string? UniDetails { get; set; }
         public string? ProfilPic_URL { get; set; }
         public string? ImgExtension { get; set; }
+        
         [NotMapped]
         public IFormFile UploadedProfilePic { get; set; }
         //public string UserId { get; set; }
