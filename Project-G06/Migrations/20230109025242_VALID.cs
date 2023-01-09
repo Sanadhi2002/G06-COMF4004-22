7882a799539b4f347c1a192a298e9dd5715cbbef
janithDakshitha <107628399+janithDakshitha@users.noreply.github.com>
2023-01-09 11:00:25 +0530

    update models

---
+++ Project-G06/Migrations/20230109025242_VALID.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectG06.Migrations.WebApplication2Db
+{
+    /// <inheritdoc />
+    public partial class VALID : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UniShortname",
+                table: "UniProfileModel",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UniFullname",
+                table: "UniProfileModel",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DegreeContents",
+                table: "DegreeModel",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DegreeContents",
+                table: "DegreeModel");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UniShortname",
+                table: "UniProfileModel",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UniFullname",
+                table: "UniProfileModel",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- Project-G06/Models/UniProfileModel.cs
+++ Project-G06/Models/UniProfileModel.cs
@@ -9,20 +9,21 @@ namespace Project_G06.Models
         [Key, ForeignKey("WebApplication2User")]
         public string UserId { get; set; }
         [DisplayName("University short Name")]
-        
-        public string? UniShortname { get; set; }
+        [Required]
+        public string UniShortname { get; set; }
         
         [DisplayName("University Full Name")]
-        public string? UniFullname { get; set; }
+        public string UniFullname { get; set; }
         [DisplayName("University Details")]
-        public string? UniDetails { get; set; }
-        public string? ProfilPic_URL { get; set; }
+        public string UniDetails { get; set; }
+       
+        public string ProfilPic_URL { get; set; }
         public string? ImgExtension { get; set; }
-        
+        //[Required(ErrorMessage ="Please submit profile Picture")]
         [NotMapped]
         public IFormFile UploadedProfilePic { get; set; }
         //public string UserId { get; set; }
         public virtual WebApplication2User WebApplication2User { get; set; }
-        public virtual Uni_FacultyModel UniFacultyModel { get; set; }
+        
     }
 }
